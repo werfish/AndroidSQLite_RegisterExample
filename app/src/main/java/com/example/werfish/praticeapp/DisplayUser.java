deece877d3d8022d829b0ae3c4241354f567fb93
Robert Mazurowski <werfish1@gmail.com>
2017-04-17 22:21:28 +0200

    Refactored the DisplayUser class to accomodate the layout changes

--- app/src/main/java/com/example/werfish/praticeapp/DisplayUser.java
+++ app/src/main/java/com/example/werfish/praticeapp/DisplayUser.java
@@ -9,11 +9,16 @@ public class DisplayUser extends AppCompatActivity {
 
     private UsersDBHelper db;
 
+    TextView id;
     TextView name ;
     TextView email;
     TextView phone;
-    TextView pass;
-    TextView encrypted;
+    TextView plainPass;
+
+    TextView salt;
+    TextView passWithSalt;
+    TextView encryptedNoSalt;
+    TextView encryptedWithSalt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +28,16 @@ public class DisplayUser extends AppCompatActivity {
         //Make the query to the db
         db = new UsersDBHelper(this);
 
+        id = (TextView) findViewById(R.id.idDisplay);
         name = (TextView) findViewById(R.id.nameDisplay);
         email = (TextView) findViewById(R.id.emailDisplay);
         phone = (TextView) findViewById(R.id.phoneDisplay);
-        pass = (TextView) findViewById(R.id.passDisplay);
-        encrypted = (TextView) findViewById(R.id.encryptedDisplay);
+        plainPass = (TextView) findViewById(R.id.passwordDisplay);
+
+        salt = (TextView) findViewById(R.id.saltDisplay);
+        passWithSalt = (TextView) findViewById(R.id.passWithSaltDisplay);
+        encryptedNoSalt = (TextView) findViewById(R.id.hashNoSaltDisplay);
+        encryptedWithSalt = (TextView) findViewById(R.id.hashWithSaltDisplay);
 
         Bundle extras = getIntent().getExtras();
         if(extras !=null){
@@ -36,11 +46,16 @@ public class DisplayUser extends AppCompatActivity {
             Cursor rs = db.getData(Value);
             rs.moveToFirst();
 
+            id.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_ID)));
             name.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_NAME)));
             email.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_EMAIL)));
             phone.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_PHONE)));
-            pass.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_PASSWORD)));
-            encrypted.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_ENCRYPTED_PASS)));
+            plainPass.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_PASSWORD)));
+
+            salt.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_SALT));
+            passWithSalt.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_AFTER_SALT)));
+            encryptedNoSalt.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_ENCRYPTED_NOSALT)));
+            encryptedWithSalt.setText(rs.getString(rs.getColumnIndex(UsersDBHelper.USERS_COLUMN_ENCRYPTED_PASS)));
 
             if (!rs.isClosed())  {
                 rs.close();
